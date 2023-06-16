b438eb8d06a4c3cdd512f0d29f357d7c643302e2
somaiahkm <125279140+somaiahkm@users.noreply.github.com>
2023-06-16 07:03:55 +0530

    Update generate.py

--- PythonFiles/generate.py
+++ PythonFiles/generate.py
@@ -1,1 +1,11 @@
+from sklearn.datasets import fetch_openml
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn import metrics
+import pickle 
 
+mnist = fetch_openml('mnist_784', version=1, as_frame=False)
+X, y = mnist["data"], mnist["target"]
+X_train, X_test, y_train, y_test = X[:60000], X[60000:], y[:60000], y[60000:]
+knn_clf = KNeighborsClassifier(n_neighbors=4, weights='distance')
+knn_clf.fit(X_train, y_train)
+pickle.dump(knn_clf1, open('pickle.pkl', 'wb'))
