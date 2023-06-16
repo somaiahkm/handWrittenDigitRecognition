665fdeba64620e0d1ac0966dcea0e94401309550
somaiahkm <125279140+somaiahkm@users.noreply.github.com>
2023-06-16 07:04:37 +0530

    Update generate.py

--- PythonFiles/generate.py
+++ PythonFiles/generate.py
@@ -8,4 +8,4 @@ X, y = mnist["data"], mnist["target"]
 X_train, X_test, y_train, y_test = X[:60000], X[60000:], y[:60000], y[60000:]
 knn_clf = KNeighborsClassifier(n_neighbors=4, weights='distance')
 knn_clf.fit(X_train, y_train)
-pickle.dump(knn_clf1, open('pickle.pkl', 'wb'))
+pickle.dump(knn_clf1, open('model_pickle.pkl', 'wb'))
